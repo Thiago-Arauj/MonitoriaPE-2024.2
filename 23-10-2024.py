27203ba76426f5c587e747d42ab65a99f8358e69
Thiago Araujo <thiagorodrigues12372@gmail.com>
2024-10-23 14:24:22 -0300

    aula referente a lista e loops

---
+++ 23-10-2024.py
@@ -0,0 +1,57 @@
+# Lista e seus metodos
+lista = [1, 2, True, "string"]
+a = 10
+# adiciona no final
+lista.append(a)
+# adiciona no indice que especificar
+lista.insert(0, a)
+# remove do final mas também pode remover um indice especifico por exemplo .pop(3) para remover o indice 3
+lista.pop()
+# remove por valor, sem olhar para indice
+lista.remove(2)
+# retorna a posição (index) de um item na lista
+lista.index("string")
+
+print(lista)
+
+# While
+
+cont = 1
+par = []
+impar = []
+
+# Fazendo assim eu tiro o controle do loop dele mesmo e o delego para o bloco if
+while True:
+
+    if cont % 2 == 0:
+        par.append(cont)
+    else:
+        impar.append(cont)
+
+    cont += 1
+
+    if cont > 200:
+        break
+
+par.extend(impar)
+# par.append(impar)
+
+
+# For
+
+# loop definido, preciso dizer qual objeto quero que ele percorra, nesse caso ele percorre um range do tamanho da lista par
+for num in range(len(par)):
+    print(f"Par: {par[num]}")
+
+# aqui ele percorre a lista par assumindo todos os valores um por vez
+for k in impar:
+    print(f"Impar: {k}")
+
+text = ["Thiago", "Rodrigues"]
+
+for nome in text:
+    
+    for letra in nome:
+
+        print(letra)
+    
